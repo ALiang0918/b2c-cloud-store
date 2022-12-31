cbde9665e21223beb8b27d3618a6e7577fde46ce
aliang <aliang_ex@163.com>
2022-12-31 09:54:57 +0800

    购物车服务已完成

---
+++ store-commons/src/main/java/org/aliang/pojo/Cart.java
@@ -0,0 +1,34 @@
+package org.aliang.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@TableName(value = "cart")
+public class Cart implements Serializable {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @TableField(value = "user_id")
+    private Integer userId;
+
+    @TableField(value = "product_id")
+    private Integer productId;
+
+    @TableField(value = "num")
+    private Integer num;
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String COL_ID = "id";
+
+    public static final String COL_USER_ID = "user_id";
+
+    public static final String COL_PRODUCT_ID = "product_id";
+
+    public static final String COL_NUM = "num";
+}
